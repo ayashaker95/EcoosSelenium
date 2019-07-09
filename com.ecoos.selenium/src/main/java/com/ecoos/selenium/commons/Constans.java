eaf14d56a9444dcc7a7b57de667c8217aa4aac75
Aya Shaker <ashaker@asaltech.com>
2019-07-09 16:13:54 +0300

    test push

--- com.ecoos.selenium/src/main/java/com/ecoos/selenium/commons/Constans.java
+++
@@ -1,9 +0,0 @@
-package com.ecoos.selenium.commons;
-
-public class Constans {
-	
-	public static final String LoginErrorMsg = "Invalid Username or Password. Please Try Again!";
-	public static final String Collect = "http://172.22.1.107/dashboard/collect";
-	
-
-}
